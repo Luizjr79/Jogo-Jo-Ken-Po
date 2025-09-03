5e6b11f09fbe503b49e6107861667cb7d1f4dd10
Luizjr79 <luizjr79@outlook.com>
2025-09-02 22:51:03 -0300

    Update jokenpo.py

--- Jokenpo/jokenpo.py
+++ Jokenpo/jokenpo.py
@@ -6,6 +6,8 @@ O usuário joga contra o computador, que faz uma escolha aleatória.
 import random # Importa o módulo random para gerar escolhas aleatórias do computador
 import time # Importa o módulo time para criar um pequeno delay e dar mais dinamismo ao jogo
 
+placar = {"vitorias": 0, "derrotas": 0, "empates": 0}
+
 lista_jogadas = ['PEDRA', 'PAPEL', 'TESOURA']
 # Lista com as opções possíveis de jogadas
 
@@ -23,7 +25,7 @@ while True:
   print("KEN")
   time.sleep(0.5)
   print("PÔ!")
-  time.sleep(0.5)
+  time.sleep(0.7)
 
   print(f"Você escolheu {jogada}")
 # Mostra a escolha do usuário
@@ -36,22 +38,32 @@ while True:
 
   if jogada == jogada_bot:
       print('EMPATE!')
-    # Verifica se houve empate
+      placar["empates"] += 1 # Adiciona + 1 ao placar
+  # Verifica se houve empate
 
   elif (jogada == 'PEDRA' and jogada_bot == 'TESOURA') or \
        (jogada == 'TESOURA' and jogada_bot == 'PAPEL') or \
        (jogada == 'PAPEL' and jogada_bot == 'PEDRA'):
       print("Você VENCEU!!!")
-    # Condições em que o usuário vence
+      placar["vitorias"] += 1 
+  # Condições em que o usuário vence
 
   else:
       print("Você PERDEU :/")
-    # Se não for empate nem vitória, o usuário perde
+      placar["derrotas"] += 1
+  # Se não for empate nem vitória, o usuário perde
 
 
 # Jogo irá rodar novamente, caso o jogador deseje
 
   resposta = input("Deseja jogar novamente? (S/N): ").upper()
-  if resposta == "N":
+  if resposta == "N": 
+     print("===== PLACAR FINAL =====")
+     time.sleep(0.2)
+     print(f"Vitórias: {placar['vitorias']}")
+     time.sleep(0.2)
+     print(f"Derrotas: {placar['derrotas']}")
+     time.sleep(0.2)
+     print(f"Empates: {placar['empates']}")
      print("Obrigado por jogar. Até mais!")
      break
